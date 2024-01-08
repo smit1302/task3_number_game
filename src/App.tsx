f5395ada8b2d0424467c42617e386b9a3849a9f1
smit1302 <smitjchoksi28@gmail.com>
2024-01-08 14:34:00 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputNumber, setInputNumber] = useState<number>(0);
+  const [randomNumber, setRandomNumber] = useState<number | null>(null);
+  const [message, setMessage] = useState<string>('');
+
+
+  const handleInputNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newInputNumber = parseInt(e.target.value, 10);
+    setInputNumber(newInputNumber);
+  };
+
+  useEffect (() =>{
+    const newRandomNumber = Math.floor(Math.random() * 20) + 1;
+    setRandomNumber(newRandomNumber);
+    console.log("The random number generated is",newRandomNumber);
+  },[]);
+
+  const checkNumber = () => {
+    setMessage('');
+    if(randomNumber===inputNumber)
+    {setMessage('You have guessed the correct number');}
+    else if(randomNumber!==null&&randomNumber>inputNumber)
+    {setMessage('The number you have guessed is less than the correct number');}
+    else if(randomNumber!==null&&randomNumber<inputNumber)
+    {setMessage('The number you have guessed is more than the correct number');}
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Guess the number</h1>
+      <input
+        type="number"
+        id="inputNumber"
+        value={inputNumber}
+        onChange={handleInputNumber}
+        placeholder="Enter the number"
+      />
+      <p>{message}</p>
+      <button onClick={checkNumber}>Check</button>
     </div>
   );
 }
